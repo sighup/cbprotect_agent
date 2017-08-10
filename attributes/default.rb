a2ff7d0bfdd380a022796f26278181c3ee8a8142
Matt Saliano <sighup@gmail.com>
2017-08-10 12:21:19 -0500

    variable name adjustment for clarity/consistency

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['cbprotect_agent']['source_url'] = value_for_platform_family(
+  'windows'  => 'https://cbprotect/hostpkg/pkg.php?pkg=Disabled%20Policy.msi',
+  'rhel'     => 'https://cbprotect/hostpkg/pkg.php?pkg=Disabled%20Policy-redhat.tgz'
+  )
